21a32703155e06aa6949e07da83f62879169ddcf
NguyenTanDat_20130221 <20130221@st.hcmuaf.edu.vn>
2022-11-03 18:06:31 +0700

    cập nhật trang admin: xóa những thành phần không cần thiết, thêm chức năng đăng nhập với tài khoản ảo, giảm dung lượng files

--- admin/js/login.js
+++ admin/js/login.js
@@ -1,115 +1,66 @@
-/* =========================================== */
-/* =========================================== */
-function validate() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password-field").value;
-    //Đặt 1 Admin ảo để đăng nhập quản trị
-    if (username == "admin" && password == "123456") {
-        swal({
-            title: "",
-            text: "Xin chào Admin",
-            icon: "success",
-            close: true,
-            button: false,
-        });
-        window.location = "doc/index.html";
-        return true;
+(function () {
+    'use strict';
 
-    }
-    //Nếu không nhập gì mà nhấn đăng nhập thì sẽ báo lỗi
-    if (username == "" && password == "") {
-        swal({
-            title: "",
-            text: "Bạn chưa điền đầy đủ thông tin đăng nhập...",
-            icon: "error",
-            close: true,
-            button: "Thử lại",
+    // Show/Hide password
+    $(function () {
+        $('.click-eye').click(function () {
+            $(this).toggleClass('bx-show bx-hide')
+            var input = $($(this).attr('toggle'))
+            if (input.attr('type') == 'password')
+                input.attr('type', 'text')
+            else
+                input.attr('type', 'password')
         });
+    })
 
-        return false;
+    // Login
+    $(function () {
+        $('#btn-login').click(function () {
+            var username = $('#field-username').val()
+            var password = $('#field-password').val()
 
-    }
-    //Nếu không nhập mật khẩu mà đúng tài khoản 
-    if (username == "admin" && password == "") {
-        swal({
-            title: "",
-            text: "Bạn chưa nhập mật khẩu...",
-            icon: "warning",
-            close: true,
-            button: "Thử lại",
-        });
-        return false;
-    }
-    //Nếu không nhập tài khoản sẽ báo lỗi
-    if (username == null || username == "") {
-        swal({
-            title: "",
-            text: "Tài khoản đang để trống...",
-            icon: "warning",
-            close: true,
-            button: "Thử lại",
-        });
-        return false;
-    }
-    //Nếu không nhập mật khẩu sẽ báo lỗi
-    if (password == null || password == "") {
-        swal({
-            title: "",
-            text: "Mật khẩu đang để trống...",
-            icon: "warning",
-            close: true,
-            button: "Thử lại",
-        });
-        return false;
-    }
-    //Nếu trống toàn bộ thì báo lỗi
-    else {
-        swal({
-            title: "",
-            text: "Sai thông tin đăng nhập hãy kiểm tra lại...",
-            icon: "error",
-            close: true,
-            button: "Thử lại",
-        });
-        return true;
-    };
-}
+            if (username == 'admin' && password == '12345') {
+                window.location = 'index.html'
+                return true
+            } else {
+                swal({
+                    title: 'Sai thông tin đăng nhập',
+                    text: 'Vui lòng đăng nhập lại',
+                    icon: 'error',
+                    close: true,
+                    button: 'Thử lại'
+                })
+                return false
+            }
+        })
+    })
 
-/*  PHẦN NỘI DUNG KHÔI PHỤC MẬT KHẨU   */
+    // Forgot password
+    $(function () {
+        $('#btn-forgot').click(function () {
+            var email = $('#field-email').val()
+            var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-/* =========================================== */
-/* =========================================== */
-//  function validate() {
-//      var email = document.getElementById("email").value;
-//     if (email == null || email == "") {
-//        swal("Bạn Chưa Nhập Email", "Vui Lòng Kiểm Tra", "warning");
-//        return false;
-//    }
-//}
-function RegexEmail(emailInputBox) {
-    var emailStr = document.getElementById(emailInputBox).value;
-    var emailRegexStr = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    var isvalid = emailRegexStr.test(emailStr);
-    if (!isvalid) {
-        swal({
-            title: "",
-            text: "Bạn vui lòng nhập đúng định dạng email...",
-            icon: "error",
-            close: true,
-            button: "Thử lại",
-        });
+            if (!emailRegex.test(email)) {
+                swal({
+                    title: 'Định dạng email không chính xác',
+                    text: 'Vui lòng nhập lại',
+                    icon: 'error',
+                    close: true,
+                    button: 'Thử lại'
+                })
 
-        emailInputBox.focus;
-    } else {
-        swal({
-            title: "",
-            text: "Chúng tôi vừa gửi cho bạn email hướng dẫn đặt lại mật khẩu vào địa chỉ cho bạn",
-            icon: "success",
-            close: true,
-            button: "Đóng",
-        });
-        emailInputBox.focus;
-        window.location = "#";
+                return false
+            } else {
+                swal({
+                    text: 'Đang xác thực email, vui lòng đợi...',
+                    icon: 'success',
+                    close: true,
+                    buttons: false
+                })
 
-    }
-}
+                return true
+            }
+        })
+    })
+})()
