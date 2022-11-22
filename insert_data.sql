a37e6815bb99d8012ffa55a1d42b3a7832d9bd0d
Trần Minh Tuyên <114179423+TranMinhTuyen-20130459@users.noreply.github.com>
2022-11-22 15:36:13 +0700

    thêm data cho bảng status_account

--- insert_data.sql
+++ insert_data.sql
@@ -1,5 +1,4 @@
-
 -- STATUS_ACC
-insert into status_product values (1,"Bình thường");
-insert into status_product values (2,"Tạm khóa");
-insert into status_product values (3,"Khóa vĩnh viễn");
+insert into status_acc values (1,"Bình thường");
+insert into status_acc values (2,"Tạm khóa");
+insert into status_acc values (3,"Khóa vĩnh viễn");
