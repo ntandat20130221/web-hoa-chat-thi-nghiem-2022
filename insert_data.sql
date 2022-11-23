d4c127dd07aefe2b225f1647c232d5f0491c2417
Trần Minh Tuyên <114179423+TranMinhTuyen-20130459@users.noreply.github.com>
2022-11-23 10:57:45 +0700

    insert account_admin, update account_admin

--- insert_data.sql
+++ insert_data.sql
@@ -20,6 +20,31 @@ INSERT INTO account_customer(username, pass, id_status_acc, id_city, fullname, s
 VALUES("nguyentandat@gmail.com", "123", 1, 1, "Nguyễn Tấn Đạt", "Nam", "nguyentandat@gmail.com", "0353677273",
 "Bến Tre");
 
+-- ROLE_ADMIN
+insert into role_admin values (1,"root");
+insert into role_admin values (2,"member");
+
+-- ACCOUNT_ADMIN
+insert into account_admin (username,id_role_admin,id_status_acc,passwordAD) values ("tuyen",1,1,"20130459");
+insert into account_admin (username,id_role_admin,id_status_acc,passwordAD) values ("tai",2,1,"20130390");
+insert into account_admin (username,id_role_admin,id_status_acc,passwordAD) values ("dat",2,1,"20130221");
+
+-- UPDATE TABLE ACCOUNT_ADMIN
+UPDATE account_admin
+SET fullname = "Trần Minh Tuyên"
+where username ="tuyen";
+
+
+UPDATE account_admin
+SET fullname = "Nguyễn Phú Tài"
+where username ="tai";
+
+
+UPDATE account_admin
+SET fullname = "Nguyễn Tấn Đạt"
+where username ="dat";
+-------------------------------------------
+
 -- STATUS PRODUCT
 INSERT INTO status_product VALUES (1, 'Mới');
 INSERT INTO status_product VALUES (2, 'Hết');
