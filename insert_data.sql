87fa3a0e7250e29b115901e698ccb76cd2444141
NguyenTanDat-20130221 <115601753+NguyenTanDat-20130221@users.noreply.github.com>
2022-11-23 09:16:48 +0700

    thêm vào insert product

--- insert_data.sql
+++ insert_data.sql
@@ -19,3 +19,40 @@ VALUES("tranminhtuyen0908@gmail.com", "123", 1, 1, "Trần Minh Tuyên", "Nam", 
 INSERT INTO account_customer(username, pass, id_status_acc, id_city, fullname, sex, email_customer, phone_customer, address)
 VALUES("nguyentandat@gmail.com", "123", 1, 1, "Nguyễn Tấn Đạt", "Nam", "nguyentandat@gmail.com", "0353677273",
 "Bến Tre");
+
+-- STATUS PRODUCT
+INSERT INTO status_product VALUES (1, 'Mới');
+INSERT INTO status_product VALUES (2, 'Hết');
+INSERT INTO status_product VALUES (3, 'Còn');
+
+-- TYPE PRODUCT
+INSERT INTO type_product VALUES (0,'Acid');
+INSERT INTO type_product VALUES (0, 'Bazo');
+INSERT INTO type_product VALUES (0, 'Muối');
+
+-- SUPPLIERS
+INSERT INTO suppliers VALUES (0, 'suppliers 1');
+INSERT INTO suppliers VALUES (0, 'suppliers 2');
+INSERT INTO suppliers VALUES (0, 'suppliers 3');
+
+-- PRODUCTS
+INSERT INTO products VALUES (0, 1, 1, 1, 'Acid Sulfuric 95%', 'Axit Sunfuric hay còn gọi H2S04 là một chất hóa học được mệnh danh là “ vua” của các loại hóa chất. Nó đóng vai trò chủ chốt cho sự tồn tại và phát triển của ngành công nghiệp hiện nay.', '../images/hoa_chat/acid_bazo/acid_huu_co/550x750/acid-huu-co-1.jpg', 100, 'Mỹ', null, 4);
+INSERT INTO products VALUES (0, 1, 1, 1, 'Acid Sulfuric 95%', 'Axit Sunfuric hay còn gọi H2S04 là một chất hóa học được mệnh danh là “ vua” của các loại hóa chất. Nó đóng vai trò chủ chốt cho sự tồn tại và phát triển của ngành công nghiệp hiện nay.', '../images/hoa_chat/acid_bazo/acid_huu_co/550x750/acid-huu-co-1.jpg', 100, 'Mỹ', null, 4);
+INSERT INTO products VALUES (0, 1, 1, 1, 'Acid Sulfuric 95%', 'Axit Sunfuric hay còn gọi H2S04 là một chất hóa học được mệnh danh là “ vua” của các loại hóa chất. Nó đóng vai trò chủ chốt cho sự tồn tại và phát triển của ngành công nghiệp hiện nay.', '../images/hoa_chat/acid_bazo/acid_huu_co/550x750/acid-huu-co-1.jpg', 100, 'Mỹ', null, 4);
+INSERT INTO products VALUES (0, 1, 1, 1, 'Acid Sulfuric 95%', 'Axit Sunfuric hay còn gọi H2S04 là một chất hóa học được mệnh danh là “ vua” của các loại hóa chất. Nó đóng vai trò chủ chốt cho sự tồn tại và phát triển của ngành công nghiệp hiện nay.', '../images/hoa_chat/acid_bazo/acid_huu_co/550x750/acid-huu-co-1.jpg', 100, 'Mỹ', null, 4);
+INSERT INTO products VALUES (0, 1, 1, 1, 'Acid Sulfuric 95%', 'Axit Sunfuric hay còn gọi H2S04 là một chất hóa học được mệnh danh là “ vua” của các loại hóa chất. Nó đóng vai trò chủ chốt cho sự tồn tại và phát triển của ngành công nghiệp hiện nay.', '../images/hoa_chat/acid_bazo/acid_huu_co/550x750/acid-huu-co-1.jpg', 100, 'Mỹ', null, 4);
+INSERT INTO products VALUES (0, 1, 1, 1, 'Acid Sulfuric 95%', 'Axit Sunfuric hay còn gọi H2S04 là một chất hóa học được mệnh danh là “ vua” của các loại hóa chất. Nó đóng vai trò chủ chốt cho sự tồn tại và phát triển của ngành công nghiệp hiện nay.', '../images/hoa_chat/acid_bazo/acid_huu_co/550x750/acid-huu-co-1.jpg', 100, 'Mỹ', null, 4);
+INSERT INTO products VALUES (0, 1, 1, 1, 'Acid Sulfuric 95%', 'Axit Sunfuric hay còn gọi H2S04 là một chất hóa học được mệnh danh là “ vua” của các loại hóa chất. Nó đóng vai trò chủ chốt cho sự tồn tại và phát triển của ngành công nghiệp hiện nay.', '../images/hoa_chat/acid_bazo/acid_huu_co/550x750/acid-huu-co-1.jpg', 100, 'Mỹ', null, 4);
+INSERT INTO products VALUES (0, 1, 1, 1, 'Acid Sulfuric 95%', 'Axit Sunfuric hay còn gọi H2S04 là một chất hóa học được mệnh danh là “ vua” của các loại hóa chất. Nó đóng vai trò chủ chốt cho sự tồn tại và phát triển của ngành công nghiệp hiện nay.', '../images/hoa_chat/acid_bazo/acid_huu_co/550x750/acid-huu-co-1.jpg', 100, 'Mỹ', null, 4);
+INSERT INTO products VALUES (0, 1, 1, 1, 'Acid Sulfuric 95%', 'Axit Sunfuric hay còn gọi H2S04 là một chất hóa học được mệnh danh là “ vua” của các loại hóa chất. Nó đóng vai trò chủ chốt cho sự tồn tại và phát triển của ngành công nghiệp hiện nay.', '../images/hoa_chat/acid_bazo/acid_huu_co/550x750/acid-huu-co-1.jpg', 100, 'Mỹ', null, 4);
+
+-- PRICE PRODUCT
+INSERT INTO price_product VALUES (2, '2022-12-15', 12000000, 14000000);
+INSERT INTO price_product VALUES (3, '2022-12-15', 12000000, 14000000);
+INSERT INTO price_product VALUES (4, '2022-12-15', 12000000, 14000000);
+INSERT INTO price_product VALUES (5, '2022-12-15', 12000000, 14000000);
+INSERT INTO price_product VALUES (6, '2022-12-15', 12000000, 14000000);
+INSERT INTO price_product VALUES (7, '2022-12-15', 12000000, 14000000);
+INSERT INTO price_product VALUES (8, '2022-12-15', 12000000, 14000000);
+INSERT INTO price_product VALUES (9, '2022-12-15', 12000000, 14000000);
+INSERT INTO price_product VALUES (10, '2022-12-15', 12000000, 14000000);
