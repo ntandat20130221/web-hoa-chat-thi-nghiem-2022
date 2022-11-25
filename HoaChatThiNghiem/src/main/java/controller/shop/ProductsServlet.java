f76f238f656e02f632c1ed8efba7ebdacab3824e
NguyenTanDat-20130221 <20130221@st.hcmuaf.edu.vn>
2022-11-25 16:36:25 +0700

    cÃập nhÃật giao diÃện

--- HoaChatThiNghiem/src/main/java/controller/shop/ProductsServlet.java
+++ HoaChatThiNghiem/src/main/java/controller/shop/ProductsServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -21,27 +22,40 @@ public class ProductsServlet extends HttpServlet {
 
         String minPrice = req.getParameter("minPrice");
         String maxPrice = req.getParameter("maxPrice");
-        double min = Double.parseDouble(minPrice);
-        double max = Double.parseDouble(maxPrice);
+        double min = Double.MIN_VALUE, max = Double.MAX_VALUE;
+        if (minPrice != null) {
+            min = Double.parseDouble(minPrice);
+        }
+
+        if (maxPrice != null) {
+            max = Double.parseDouble(maxPrice);
+        }
+
+        List<Product> filteredProducts = new ArrayList<>();
+        for (Product product : products) {
+            if (product.getNewPrice() >= min && product.getNewPrice() <= max) {
+                filteredProducts.add(product);
+            }
+        }
 
         String param = req.getParameter("sort");
         if (param != null) {
             switch (param) {
                 case "name":
-                    products.sort(Comparator.comparing(Product::getName));
+                    filteredProducts.sort(Comparator.comparing(Product::getName));
                     break;
                 case "price-up":
-                    products.sort(Comparator.comparing(Product::getNewPrice));
+                    filteredProducts.sort(Comparator.comparing(Product::getNewPrice));
                     break;
                 case "price-down":
-                    products.sort((o1, o2) -> (int) (o2.getNewPrice() - o1.getNewPrice()));
+                    filteredProducts.sort((o1, o2) -> (int) (o2.getNewPrice() - o1.getNewPrice()));
                     break;
             }
         } else {
-            products.sort(Comparator.comparing(Product::getName));
+            filteredProducts.sort(Comparator.comparing(Product::getName));
         }
 
-        req.setAttribute("products", products);
+        req.setAttribute("products", filteredProducts);
         req.getRequestDispatcher("products.jsp").forward(req, resp);
     }
 }
