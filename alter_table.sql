74110c58592de6f67ca31133a22716dd2458d0b0
Trần Minh Tuyên <114179423+TranMinhTuyen-20130459@users.noreply.github.com>
2022-12-05 13:33:38 +0700

    thêm cột trong database

--- alter_table.sql
+++ alter_table.sql
@@ -1,3 +1,18 @@
-
+-- ACCOUNT_ADMIN
 alter table account_admin
 			add column fullname varchar(255)
+			
+-- PRODUCTS
+ALTER TABLE products 
+			DROP COLUMN country;
+			
+ALTER TABLE products
+			ADD COLUMN nameAdmin VARCHAR(50);
+
+-- PRICE_PRODUCT
+ALTER TABLE price_product
+			MODIFY COLUMN date TIMESTAMP DEFAULT(CURRENT_TIMESTAMP);
+		
+ALTER TABLE price_product
+			ADD COLUMN nameAdmin VARCHAR(50);
+			
