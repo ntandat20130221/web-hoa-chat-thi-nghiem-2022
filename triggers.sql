db9de1a441eb488ca4a00ac46136e65b33351466
NguyenTanDat-20130221 <115601753+NguyenTanDat-20130221@users.noreply.github.com>
2022-12-19 22:50:27 +0700

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -11,3 +11,9 @@ BEGIN
 	IF NEW.stars > 5 OR NEW.stars < 1 THEN SIGNAL SQLSTATE '45001' SET message_text = 'stars must be in 1 - 5';
 	END IF;
 END;
+
+CREATE TRIGGER `trigger_sold`
+AFTER INSERT ON `bill_detail` FOR EACH ROW
+BEGIN	
+	UPDATE `sold_product` SET quantity_sold = (SELECT SUM(quantity) FROM bill_detail WHERE id_product = NEW.id_product) WHERE id_product = NEW.id_product;
+END;
