3fc7f82979ba74bf531b0637319c707a782652b1
tuyen <20130459@st.hcmuaf.edu.vn>
2023-01-06 22:32:56 +0700

    thêm chức năng quản lý admin

--- HoaChatThiNghiem/src/main/java/service/AdminService.java
+++ HoaChatThiNghiem/src/main/java/service/AdminService.java
@@ -1,10 +1,8 @@
 package service;
 
 import database.DbConnection;
-import model.Admin;
-import model.Bill;
-import model.Customer;
-import model.Product;
+import database.dao.AdminDAO;
+import model.*;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -66,6 +64,31 @@ public class AdminService {
             connectDB.close();
         }
         return null;
+    }
+
+    public static List<Admin> getAllAdmin() {
+        DbConnection connectDB = DbConnection.getInstance();
+        AdminDAO dao = new AdminDAO();
+        try {
+            return dao.getAllAdmin(connectDB);
+        } finally {
+            connectDB.close();
+        }
+    }
+
+    public static List<Object> getAllRoleAdminAndStatusAcc() {
+        List<Object> result = new ArrayList<>();
+        DbConnection connectDB = DbConnection.getInstance();
+        AdminDAO dao = new AdminDAO();
+        try {
+            List<RoleAdmin> roleAdmins = dao.getAllRoleAdmin(connectDB);
+            List<StatusAcc> statusAccs = dao.getAllStatusAcc(connectDB);
+            result.add(roleAdmins);
+            result.add(statusAccs);
+        } finally {
+            connectDB.close();
+        }
+        return result;
     }
 
     private static int getQuantityByBillId(int id) {
